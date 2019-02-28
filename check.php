977e75a0fbd0af261b61ab81fab45b11a2399d2a
YusukeHatta <baiogann135@gmail.com>
2019-02-28 23:41:45 +0900

    ２回目の修正です

--- check.php
+++ check.php
@@ -12,11 +12,15 @@
       <form method="post" action="sent.php">
       <div class="form-item">■ 名前</div>
       <?php echo $_POST['name']; ?>
+      <input type="hidden" name="name" value="<?php echo $_POST['name']; ?>">
       <div class="form-item">■ メールアドレス</div>
       <?php echo $_POST['email']; ?>
+      <input type="hidden" name="email" value="<?php echo $_POST['email']; ?>">
       <div class="form-item">■ お問い合わせ内容</div>
-      <?php echo $_POST['body']; ?>
+　　　　<?php echo $_POST['body']; ?>
+      <input type="hidden" name="body" value="<?php echo $_POST['body']; ?>">
       <input type="submit" value="送信">
+
     </form>
     </div>
   </div>
--- sent.php
+++ sent.php
@@ -6,11 +6,47 @@
   <link rel="stylesheet" type="text/css" href="stylesheet.css">
 </head>
 <body>
-
-
 <div class="main">
+
+<?php
+// データベース名の定義
+define('DB_DATABASE', 'form_db');
+// データベースユーザー名の定義
+define('DB_USERNAME', 'root');
+// データベースユーザーの接続パスワードの定義
+define('DB_PASSWORD', 'Sogekinngu135@');
+// DSN（データソースネーム）の定義
+define('DB_DSN', 'mysql:host=localhost;charset=utf8;dbname='.DB_DATABASE);
+
+try {
+    // コンストラクタ作成の際に、DSN・ユーザー名・パスワード情報を渡す
+    $db = new PDO(DB_DSN, DB_USERNAME, DB_PASSWORD);
+    // エラーが起きた際にExceptionを出力する設定
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // クエリ実行
+    $statement = $db->prepare("INSERT INTO user(name, email, body) VALUES(?, ?, ?);");
+
+    $statement->execute(['山田花子', 'yamada@example.com', 'お問い合わせ内容文']);
+    
+    echo "Inserted Id : ".$db->lastInsertId();
+} catch (PDOException $e) {
+
+    var_dump($e);
+
+    exit;
+}
+
+?>
   <div class="contact-form">
+
   <h2>お問い合わせいただきありがとうございます。</h2>
+
+  <div class="form-item">■ 名前</div>
+  <?php echo $_POST['name']; ?>
+  <div class="form-item">■ メールアドレス</div>
+  <?php echo $_POST['email']; ?>
+  <div class="form-item">■ お問い合わせ内容</div>
+  <?php echo $_POST['body']; ?>
 
  <?php
  mb_language("Japanese");
